555cffdec88ffb66cf783dc9e690fa2a70d19624
Ivanilson Melo <meloivanilson@gmail.com>
2017-04-09 20:29:36 +0000

    Search widget icon

--- resources/views/partials/searchwidget.blade.php
+++ resources/views/partials/searchwidget.blade.php
@@ -1,5 +1,6 @@
 <form action="{{ action('AppController@index') }}" method="get">
-    <div class="form-group form-group mb-2 mt-2">
+    <div class="input-group mb-2 mt-2">
         <input type="text" name="q" id="q" class="form-control search-input" value="@if(isset($query)) {{ $query }} @endif" placeholder="Search for an app, or paste a URL from the App Store or Google Play store.">
+        <span class="input-group-addon search-icon"><span class="fa fa-search"></span></span>
     </div>
 </form>
